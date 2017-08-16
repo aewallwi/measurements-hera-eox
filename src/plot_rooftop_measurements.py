993174eb586c26432649d4d23388f244d0d5617e
Aaron Ewall-Wice <aaronew@mit.edu>
2017-08-16 17:10:49 -0400

    comparing both hybrid couplers in plot_rooftop_measurements.py

--- src/plot_rooftop_measurements.py
+++ src/plot_rooftop_measurements.py
@@ -5,13 +5,39 @@ import matplotlib.pyplot as plt
 
 #load measurements
 hybrid_coupler_A=AM()
+hybrid_coupler_B=AM()
 hybrid_coupler_A.read_files('../Rooftop_Antenna_Measurements_August_10th/','_hybrid_A_port_A_antenna',
                             '../hybrid_coupler_A_Sierra/A_','','ANRITSU_CSV',
                             '1','3','4',0.05,0.25)
-#load Jianshu's data
+hybrid_coupler_B.read_files('../Rooftop_Antenna_Measurements_August_10th/','_hybrid_B_port_A_antenna',
+                            '../hybrid_coupler_B/1','','ANRITSU_CSV',
+                            '1','3','4',0.05,0.25)
 
-db_s11_corr=10.*np.log10(np.abs(hybrid_coupler_A.antenna_gain_corrected_frequency))
-db_s11=10.*np.log10(np.abs(hybrid_coupler_A.antenna_raw.gainFrequency))
+
+#load Jianshu's data
+'''
+impedance_sim_amp=np.loadtxt(('../Rooftop_Antenna_Measurements_August_10th/'
+                              'Simulation/simulation_impedance_rooftop_amp.txt'))
+impedance_sim_pha=np.loadtxt(('../Rooftop_Antenna_Measurements_August_10th/'
+                              'Simulation/simulation_impedance_rooftop_pha.txt'))
+sim_impedance=impedance_sim_amp[:,1]*np.exp(1j*impedance_sim_pha[:,1])
+sim_freq=impedance_sim_amp[:,0]
+sim_s11=(sim_impedance-100)/(sim_impedance+100)
+fig0=plt.figure()
+ax0=fig0.add_axes([.1,.1,.8,.8])
+ax0.plot(sim_impedance.real)
+ax0.plot(sim_impedance.imag)
+'''
+
+
+db_s11_A_corr=10.*np.log10(np.abs(hybrid_coupler_A.antenna_gain_corrected_frequency))
+db_s11_A=10.*np.log10(np.abs(hybrid_coupler_A.antenna_raw.gainFrequency))
+db_s11_B_corr=10.*np.log10(np.abs(hybrid_coupler_B.antenna_gain_corrected_frequency))
+db_s11_B=10.*np.log10(np.abs(hybrid_coupler_B.antenna_raw.gainFrequency))
+
+
+#db_sim=10.*np.log10(np.abs(sim_s11))
+
 
 fig1=plt.figure()
 fig2=plt.figure()
@@ -20,20 +46,25 @@ ax2=fig2.add_axes([.1,.1,.8,.8])
 
 
 
-ax1.plot(hybrid_coupler_A.fAxis,db_s11)
-ax1.plot(hybrid_coupler_A.fAxis,db_s11_corr)
+ax1.plot(hybrid_coupler_A.fAxis,db_s11_A,color='k',ls='--')
+ax1.plot(hybrid_coupler_A.fAxis,db_s11_A_corr,color='k')
+ax1.plot(hybrid_coupler_B.fAxis,db_s11_B,color='grey',ls='--')
+ax1.plot(hybrid_coupler_B.fAxis,db_s11_B_corr,color='grey')
+
+#ax1.plot(sim_freq,db_sim)
 
 plotstrs=['s11','s1d','sd1','sdd','s1c','sc1','scc','scd','sdc']
-plotstrs=['s11','s12','s13','s21','s22','s23','s31','s32','s33']
+#plotstrs=['s11','s12','s13','s21','s22','s23','s31','s32','s33']
 #plotstrs=['s11']
 for dstring in plotstrs:
     print dstring
-    y=np.abs(hybrid_coupler_A.balun.get_ss(dstring))
+    y=np.abs(hybrid_coupler_A.balun.get_ds(dstring))
     print y
     y=10.*np.log10(y)
     x=hybrid_coupler_A.balun.fAxis
     ax2.plot(x,y,label=dstring)
 ax2.legend(loc='best')
+ax2.grid()
     
 
 plt.show()
