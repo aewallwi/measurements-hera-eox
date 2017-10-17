691c8f3833c68b0fb1dcbcb70a9986796d10769e
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2017-10-17 09:59:56 -0600

    updated impedance extraction.

--- src/extract_impedance.py
+++ src/extract_impedance.py
@@ -3,26 +3,26 @@ import gainData as gd
 import argparse
 parser=argparse.ArgumentParser(description='Extract antenna impedance')
 
-parser.add_argument('--snot','-s',dest='snot',dtype=str)
-parser.add_argument('--znot','-z',dest='znot',dtype=str)
-parser.add_argument('--isant','-a',dest='isant',dtype=str,
+parser.add_argument('--snot','-s',dest='snot')
+parser.add_argument('--znot','-z',dest='znot')
+parser.add_argument('--isant','-a',dest='isant',
                     help=('if "True", will assume that the provided z0'
-                          'is the antenna impedance and will retrieve
-                          termination'))
-parser.add_argument('--ftype','-f',dest='filetype',dtype=str,
+                          'is the antenna impedance and will retrieve'
+                          'termination'))
+parser.add_argument('--ftype','-f',dest='filetype',
                     help=('type of file in S11'))
-parser.add_argument('--fmin','-l',dest='fmin',dtype=float,
+parser.add_argument('--fmin','-l',dest='fmin',
                     help=('lower frequency in GHz'))
-parser.add_argument('--fmax','-u',dest='fmax',dtype=float,
+parser.add_argument('--fmax','-u',dest='fmax',
                     help=('upper frequency in GHz'))
-parser.add_argument('--output','-o',dest='output',dtype=strk
+parser.add_argument('--output','-o',dest='output',
                     help=('name of output file'))
 
 args=parser.parse_args()
-znot=parser.znot
-snot=parser.snot
-isant=parser.isant
-ftype=parser.filetype
+znot=args.znot
+snot=args.snot
+isant=args.isant
+ftype=args.filetype
 if isant=="True":
     isant=True
 else:
@@ -30,16 +30,16 @@ else:
 if not("s1p" in znot or 'z1p' in znot or "txt" in znot):
     znot=float(znot)
 g=gd.GainData()
-g.read_files(snot,ftype,args.flow,args.fmax)
+g.read_files(snot,ftype,float(args.fmin),float(args.fmax))
 zout=g.get_impedance(znot,isant=isant)
-fout=g.fAxis()
+fout=g.fAxis
 f=open(args.output,'wb')
 f.write(('! Touchstone file spoofed by extract_impedance.py \n'
          '! Z11=%s \n'
          '! S11=%s \n'
          '! Tree Item\n'
-         '# MHZ Z RI R 1 \n'))%(znot,snot)
-for chan in range(len(fout.shape[0])):
+         '# MHZ Z RI R 1 \n')%(znot,snot))
+for chan in range(fout.shape[0]):
     f.write('%.5f\t%.5f\t%.5f\n'%(fout[chan],zout[chan].real,zout[chan].imag))
 f.close()
 
